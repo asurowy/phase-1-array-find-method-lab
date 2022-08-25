684658f20c8c95f2a51c67c7510e593ceb3e036f
asurowy <101614024+asurowy@users.noreply.github.com>
2022-08-25 11:52:49 -0400

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function superbowlWin(record){
+ const wins = record.find(footballGame => footballGame.result === "W");
+console.log(wins);
+    if (wins === undefined){
+        return undefined;
+    } else {
+        return wins.year;
+    }
+    // if (wins.result === "W"){
+    //     return wins.year;
+    // }
+}
+
+
+
+
+
+
+
